37e6a95ca6b9a15d3afd2a2faf163716020226c0
K.Takahashi <nike.taka@gmail.com>
2016-03-07 11:43:25 +0900

    課題３−５まで実装

--- car.php
+++ car.php
@@ -1,14 +1,38 @@
 <?php
 
 class Car {
-    public $type = "";
-    public $gasoline = 30;
+    private $type;
+    private $gasoline = 30;
     public $mileage = 0;
 
     public function __construct($type, $gasoline)
     {
         $this->type = $type;
         $this->gasoline = $gasoline;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+    public function setType($type = null)
+    {
+        if ($type !== null)
+        {
+            $this->type = $type;
+        }
+    }
+
+    public function getGasoline()
+    {
+        return $this->gasoline;
+    }
+    public function setGasoline($gasoline)
+    {
+        if (is_numeric($gasoline))
+        {
+            $this->gasoline = $gasoline;
+        }
     }
 
     public function go()
@@ -22,14 +46,22 @@ class Car {
     }
 }
 
+// $honda = new Car('ミライース', 30);
+// var_dump($honda);
+// echo '<hr>';
+
+// $toyota = new Car('アクア', 40);
+// var_dump($toyota);
+// echo '<hr>';
+
+// $nissan = new Car('デイズ', 50);
+// var_dump($nissan);
+// echo '<hr>';
+
 $honda = new Car('ミライース', 30);
-var_dump($honda);
-echo '<hr>';
 
-$toyota = new Car('アクア', 40);
-var_dump($toyota);
-echo '<hr>';
+echo $honda->getType(); // ミライース
 
-$nissan = new Car('デイズ', 50);
-var_dump($nissan);
-echo '<hr>';
+$honda->setType('オデッセイ');
+
+echo $honda->getType(); // オデッセイ
